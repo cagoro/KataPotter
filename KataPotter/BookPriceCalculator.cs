626bb0ff9468d35108c09ad54f446be6c2ce2e13
Carlos Gonzalez Rodriguez <cgonzalezrodriguez@vistaprint.com>
2016-08-02 14:18:41 +0200

    TDD session with Dani and Pranav, the test not passing is the last test we believe we need to complete the Kata

---
+++ KataPotter/BookPriceCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace KataPotter
+{
+    public class BookPriceCalculator
+    {
+        private const decimal SINGLE_BOOK_PRICE = 8;
+
+        private readonly Dictionary<int,decimal> _setDiscount = new Dictionary<int, decimal>()
+        {
+            {1, 1}, {2, 0.95m}, {3, 0.90m}, {4, 0.80m}, {5, 0.75m}
+        }; 
+
+        public decimal GetBookPrices(List<int> books)
+        {
+            int distincBookCount = books.Count;
+
+            decimal discount = _setDiscount[distincBookCount];
+
+            int setCount = books.Min();
+
+            decimal price = setCount * distincBookCount * SINGLE_BOOK_PRICE * discount;
+
+            int remainingBooks = books.Sum() - distincBookCount * setCount;
+
+            return price + remainingBooks * SINGLE_BOOK_PRICE;
+        }
+    }
+}
---
+++ KataPotterTests/BookPriceCalculatorShould.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using KataPotter;
+using NUnit.Framework;
+
+namespace KataPotterTests
+{
+    [TestFixture]
+    public class BookPriceCalculatorShould
+    {
+        private BookPriceCalculator _bookPriceCalculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _bookPriceCalculator = new BookPriceCalculator();
+        }
+
+        [Test]
+        public void return_non_discounted_price_for_a_single_book()
+        {
+            var price = _bookPriceCalculator.GetBookPrices(new List<int> { 1 });
+
+            Assert.AreEqual(8m, price);
+        }
+
+        [Test]
+        public void return_sum_of_prices_for_same_book()
+        {
+            var price = _bookPriceCalculator.GetBookPrices(new List<int> { 2 });
+
+            Assert.AreEqual(16m, price);
+        }
+
+        [Test]
+        public void return_five_percent_discount_for_two_different_books()
+        {
+            var price = _bookPriceCalculator.GetBookPrices(new List<int> {1, 1});
+
+            Assert.AreEqual(15.2m, price);
+        }
+
+        [Test]
+        public void return_ten_percent_discount_for_three_different_books()
+        {
+            var price = _bookPriceCalculator.GetBookPrices(new List<int> { 1, 1, 1 });
+
+            Assert.AreEqual(21.6m, price);
+        }
+
+        [Test]
+        public void return_twenty_percent_discount_for_four_different_books()
+        {
+            var price = _bookPriceCalculator.GetBookPrices(new List<int> { 1, 1, 1, 1 });
+
+            Assert.AreEqual(32d - 6.4d, price);
+        }
+
+
+        [Test]
+        public void not_discount_prices_on_two_duplicated_book()
+        {
+            var price = _bookPriceCalculator.GetBookPrices(new List<int> { 3, 4});
+
+            Assert.AreEqual(53.6m, price);
+        }
+
+        [Test]
+        public void not_discount_prices_on_three_duplicated_book()
+        {
+            var price = _bookPriceCalculator.GetBookPrices(new List<int> {3, 4, 3});
+            Assert.AreEqual(72.8m, price);
+        }
+
+
+
+        [Test]
+        public void discount_multiple_sets()
+        {
+            var price = _bookPriceCalculator.GetBookPrices(new List<int> { 3, 4, 5 });
+            Assert.AreEqual(72.8m, price);
+        }
+    }
+}
